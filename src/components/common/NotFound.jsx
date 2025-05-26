0fda496254090d31da5a9523a07046e8e8e5cb95
tlamngu <tlamngu@gmail.com>
2025-05-26 20:21:29 +0700

    Changed NotFound.jsx importing notfound -> NotFound.css

--- src/components/common/NotFound.jsx
+++ src/components/common/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Notfound.css'
+import './NotFound.css'
 import {useNavigate} from 'react-router-dom'
 export default function NotFound() {
   const navigate=useNavigate()
