42049502469222ed8a4558dd4e121a5755ee7c85
tlamngu <tlamngu@gmail.com>
2025-06-06 20:03:16 +0700

    Fixed email send

--- src/components/auth/VerifyMail.jsx
+++ src/components/auth/VerifyMail.jsx
@@ -20,15 +20,9 @@ export default function VerifyMail(props) {
       <div className="logo-container">
         <img src="/Sonify-logo.png" alt="logo" />
       </div>
-      {typeVerify == 'signup' ? <p>Thank you for joining our community!<br />
+       <p>Thank you for joining our community!<br />
         We've sent you a verification email—please note it will expire in 1 hour.<br />
-        Be sure to check your spam folder if you don't see it in your inbox!</p> :
-        <p>
-          We’ve emailed you a password recovery link, valid for 2 hours.<br/>
-          Please check your inbox and spam folder.<br/>
-          If you encounter any issues, feel free to contact our customer service team.
-        </p>
-      }
+        Be sure to check your spam folder if you don't see it in your inbox!</p>
       <button disabled>Resend Email <span>500</span></button>
       <p className='allright'>All rights reserved @ Snoify,2025</p>
     </div>
